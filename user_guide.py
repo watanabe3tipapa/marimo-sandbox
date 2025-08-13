1a07d87cf423c50dff6e9b4c7d7f13d6d3efeb6e
watanabe3tipapa <twpoet@nifty.com>
2025-08-14 08:46:56 +0900

    update

--- user_guide.py
+++ user_guide.py
@@ -49,13 +49,79 @@ def _(mo):
     df = mo.sql("SELECT * FROM my_table WHERE value > {value}")
     ```
 
-    ###📊 使い方の例
+    ###📊 おもな用途
 
     - データ分析: Marimoを使用して、データフレームを操作し、インタラクティブなビジュアライゼーションを作成できます。
 
     - 機械学習: モデルのトレーニングや評価を行い、結果をリアルタイムで確認できます。
 
     ---
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    ### ⚙️ 起動方法の例
+
+    start_marimo.sh を作成しておくと便利です。
+
+    **（事例:私のデバイスの場合）**
+
+    ```PlainText
+                            
+    #!/bin/sh
+
+    # ホームディレクトリを取得
+    HOME_DIR=$HOME
+
+    # marimo-sandbox ディレクトリのパスを作成
+    MARIMO_DIR="$HOME_DIR/github/marimo-sandbox"
+
+    # ディレクトリが存在するか確認
+    if [ -d "$MARIMO_DIR" ]; then
+        # 指定したディレクトリに移動
+        cd "$MARIMO_DIR" || exit
+
+        # 実行したいコマンドをここに書く
+        # 例: git status を実行
+        echo "現在のディレクトリ: $(pwd)"
+        uv run marimo edit user_guide.py
+    else
+        echo "marimo-sandbox ディレクトリは存在しません。"
+    fi
+
+    ```
+
+    このスクリプトをファイルに保存し、実行権限を与えてから実行することができます。
+    例えば、ファイル名を start_marimo.sh とした場合、以下のコマンドで実行できます。
+
+    **実行権限付与**
+
+    ```PlainText
+    chmod +x start_marimo.sh
+
+    ```
+
+    ターミナルでホームディレクトリ（当該shファイルがある）から
+
+    **実行**
+
+    ```bash
+    ./start_marimo.sh
+
+    ```
+
+    ---
+
+
+
+
+
+
     """
     )
     return
